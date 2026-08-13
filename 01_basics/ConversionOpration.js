cb7d72e2312324b3fd5c81a0655e2a5fd8e3d503
Divyanshu Kumar <divyanshukumar9877@gmail.com>
2026-08-13 11:48:53 +0530

    Conversion of datatype

---
+++ 01_basics/ConversionOpration.js
@@ -0,0 +1,27 @@
+let score = "Divyanshu Kumar";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score); // convert string to number
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); // NaN => Not a Number
+
+//Conversion
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Divyanshu Kumar"; // 1 => true; 0 => false
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+// "" => false;
+//  "Divyanshu Kumar" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber); // "33"
+console.log(typeof stringNumber); // string
