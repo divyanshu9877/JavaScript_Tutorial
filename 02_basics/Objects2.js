2a3c7fc2573d70010a5d28ee0cd5ee5bbaf18486
Divyanshu Kumar <divyanshukumar9877@gmail.com>
2026-08-17 21:13:57 +0530

    json talk and Object De Structure

--- 02_basics/Objects2.js
+++ 02_basics/Objects2.js
@@ -1,3 +1,6 @@
+// Singleton
+
+
 // const tinderUser = new Object()  // the output will same {} due to this is singleton object
 const tinderUser = {} // the output will same {} But this is non Singleton object
 
@@ -27,7 +30,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2, ...obj4} // spread operator ...
-console.log(obj3);
+// console.log(obj3);
 
 
 const users = [
@@ -58,10 +61,45 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedin'));
+// console.log(tinderUser.hasOwnProperty('isLoggedin'));
+
+
+
+// Object De Structuring
+
+const course = {
+    coursename: " js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseinstructor 
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+
+// API
+
+// JSON Formate
+
+// {
+//     "name": "divyanshu",
+//     "coursename": "js in hindi",  // look like object in JSON formate
+//     "price": "free"
+// }
+
+// [
+//     {},
+//     {},    // Some time give in array
+//     {}
+// ]
