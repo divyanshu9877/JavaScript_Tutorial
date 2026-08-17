058f1c7a967355c4ffb84fe202e8078fd36c0dd1
Divyanshu Kumar <divyanshukumar9877@gmail.com>
2026-08-17 20:41:54 +0530

    Object Part 2

---
+++ 02_basics/Objects2.js
@@ -0,0 +1,67 @@
+// const tinderUser = new Object()  // the output will same {} due to this is singleton object
+const tinderUser = {} // the output will same {} But this is non Singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedin = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "divyanshu",
+            lastname: "kumar"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2, ...obj4} // spread operator ...
+console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+    {
+        id: 1,
+        email: "abc@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedin'));
