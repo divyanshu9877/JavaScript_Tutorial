e1cde36f19ecf6f0c781dc0453a74f3e1e4a2a1c
Divyanshu Kumar <divyanshukumar9877@gmail.com>
2026-08-19 10:21:45 +0530

    iteration two

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+
+// while loop
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+// while loop with array
+
+// let myArray = ['flash', "batman", "suparman"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1
+// }
+
+// do-while loop     // isme pehle kaam hota hai phir check hota hai
+
+
+let score = 1 
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
