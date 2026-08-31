77552ca4b76d46e40f193ad86182ccdb3161d5f4
Divyanshu Kumar <divyanshukumar9877@gmail.com>
2026-08-31 22:49:06 +0530

    getter and setter with object

---
+++ 10_classes_and_oops/object_get_set.js
@@ -0,0 +1,18 @@
+// getter and setter with object
+
+const User = {
+    _email: 'd@dk.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
